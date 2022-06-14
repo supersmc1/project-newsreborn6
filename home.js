c9bc33fdcb3a2ec549c047ef87491dfa69a1b289
sunwanchung <sunwanchung@gmail.com>
2022-06-14 14:40:10 +0900

    완료

--- home.js
+++ home.js
@@ -3,3 +3,7 @@ const icon = document.querySelector('.icon')
 icon.addEventListener('click',function(){
   window.location = 'https://supersmc1.github.io/project-reborn/'
 })
+
+item1.addEventListener('click',function(){
+  window.location = 'https://supersmc1.github.io/project-News.reborn/'
+})
